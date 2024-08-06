25d1d0efd21a6385e7f71595776bb38fe624c8ec
pshoon25 <pshoon25@naver.com>
2024-08-06 23:19:30 +0900

    URL fix

--- src/api/api.js
+++ src/api/api.js
@@ -5,17 +5,15 @@ axios.defaults.headers = {
   "Content-Type": "application/json",
 };
 
-export const api = async (method, url, data = null) => {
+export const api = async (method, url, data) => {
   try {
     const response = await axios({
       method,
       url,
       data,
     });
-
     return response.data;
   } catch (error) {
-    console.error("API 호출 중 오류 발생:", error);
-    throw error;
+    console.error(error);
   }
 };
