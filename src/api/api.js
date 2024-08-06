c1aa232385c04f18d12cf7441fabdccc867b9694
pshoon25 <pshoon25@naver.com>
2024-08-06 22:44:38 +0900

    URL fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.withCredentials = true;
 
 export const api = async (method, url, data) => {
   const response = await axios({
     method,
-    data,
     url,
-  }).catch((e) => alert(e.response.data));
+    data,
+    headers,
+  });
   return response;
 };
