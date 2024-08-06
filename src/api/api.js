b551c273411ceef89de07845488089a2128d4fbf
pshoon25 <pshoon25@naver.com>
2024-08-06 23:26:53 +0900

    URL fix

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:8080";
 axios.defaults.headers = {
   "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "http://localhost:3000",
 };
 
 export const api = async (method, url, data) => {
