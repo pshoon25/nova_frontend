d416b335e14a6988b143d05ffec84ced61616e81
pshoon25 <pshoon25@naver.com>
2024-08-06 23:09:41 +0900

    URL fix

--- src/api/api.js
+++ src/api/api.js
@@ -3,18 +3,23 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:8080";
 axios.defaults.withCredentials = true;
 
-export const api = async (method, url, data) => {
-  const headers = {
-    "Content-Type": "application/json;charset=UTF-8",
-    Accept: "application/json",
-    "Access-Control-Allow-Origin": `http://localhost:3000`,
-    "Access-Control-Allow-Credentials": "true",
-  };
-  const response = await axios({
-    method,
-    url,
-    data,
-    headers,
-  });
-  return response;
+export const api = async (method, url, data = null) => {
+  try {
+    const headers = {
+      "Content-Type": "application/json;charset=UTF-8",
+      Accept: "application/json",
+    };
+
+    const response = await axios({
+      method,
+      url,
+      data,
+      headers,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("API 호출 중 오류 발생:", error);
+    throw error;
+  }
 };
