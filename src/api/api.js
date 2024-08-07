3be6b8dc904d0bfe0847c1561becb572a812f841
pshoon25 <pshoon25@naver.com>
2024-08-07 20:35:12 +0900

    URL fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:8080";
+const url = "http://172.18.0.2:8080";
 //const url = "http://orummmedia-backend:8080";
 
 const api = axios.create({
