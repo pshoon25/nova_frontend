e839fb89c1aac114f64b432d8cc648ee2233d611
pshoon25 <pshoon25@naver.com>
2024-08-09 20:33:08 +0900

    test

--- src/component/mng/NovaMissionManage.jsx
+++ src/component/mng/NovaMissionManage.jsx
@@ -98,7 +98,7 @@ const NovaMissionManage = () => {
   return (
     <div className="mainContainerDiv">
       <div className="missionManageDiv">
-        <h2 className="menuTitle">노바 미션 관리1341234</h2>
+        <h2 className="menuTitle">노바 미션 관리13414</h2>
         <div className="searchDiv">
           <div>
             <h3>검색어</h3>
