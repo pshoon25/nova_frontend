c64da987dfa72927c31e1b2d47dcd49a59167aa1
pshoon25 <pshoon25@naver.com>
2024-08-18 15:50:53 +0900

    Agency Manage Add

--- src/component/mng/AgencyManage.jsx
+++ src/component/mng/AgencyManage.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { DataGrid } from "@mui/x-data-grid";
 import { api } from "../../api/api.js";
+import { MenuItem, Select, FormControl, InputLabel } from "@mui/material";
 
 const AgencyManage = () => {
   const navigate = useNavigate();
   const [rows, setRows] = useState([]);
-  const [status, setStatus] = useState("idle");
-  const [error, setError] = useState(null);
+  const [updatedRows, setUpdatedRows] = useState({});
 
   // 데이터 가져오기
   useEffect(() => {
@@ -15,18 +15,14 @@ const AgencyManage = () => {
   }, []);
 
   const getAgencyList = async () => {
-    setStatus("loading");
     try {
       const response = await api.get("/agency/getAgencyList");
-      console.log(response);
-      console.log(response.data);
-
-
       const data = response.data;
 
       // API 응답에 맞게 데이터 변환
       const formattedData = data.map((agency) => ({
         id: agency.agencyCode,
+        agencyCode: agency.agencyCode || "N/A",
         loginId: agency.loginId || "N/A",
         password: agency.password || "N/A",
         agencyName: agency.agencyName || "N/A",
@@ -45,11 +41,8 @@ const AgencyManage = () => {
           : "N/A",
       }));
       setRows(formattedData);
-      setStatus("succeeded");
     } catch (error) {
-      console.error("Error fetching agency data:", error);
-      setError(error.message);
-      setStatus("failed");
+      console.error("Error:", error);
     }
   };
 
@@ -57,45 +50,127 @@ const AgencyManage = () => {
     navigate("/main/addAgency");
   };
 
+  const processRowUpdate = (newRow, oldRow) => {
+    setUpdatedRows((prev) => ({
+      ...prev,
+      [newRow.id]: { ...oldRow, ...newRow },
+    }));
+    return newRow;
+  };
+
+  const agencySave = async () => {
+    const updatedCount = Object.keys(updatedRows).length;
+
+    if (updatedCount > 0) {
+      const confirmation = window.confirm(
+        `수정된 ${updatedCount}개의 항목이 있습니다. 저장하시겠습니까?`
+      );
+
+      if (confirmation) {
+        await agencySaveApi(Object.values(updatedRows));
+        setUpdatedRows({});
+      }
+    } else {
+      alert("수정된 항목이 없습니다.");
+    }
+  };
+
+  const agencySaveApi = async (updatedData) => {
+    try {
+      await api.put("/agency/updateAgencyInfo", updatedData);
+      alert("저장되었습니다.");
+      getAgencyList();
+    } catch (error) {
+      console.error("Error:", error);
+      alert("저장 중 오류가 발생했습니다.");
+    }
+  };
+
+  // 드롭다운 렌더링 함수
+  const renderDropdownEditCell = (params) => {
+    return (
+      <FormControl fullWidth>
+        <InputLabel>{params.field}</InputLabel>
+        <Select
+          value={params.value}
+          onChange={(event) =>
+            params.api.setEditCellValue({
+              ...params,
+              value: event.target.value,
+            })
+          }
+          autoWidth
+        >
+          <MenuItem value="Y">Y</MenuItem>
+          <MenuItem value="N">N</MenuItem>
+        </Select>
+      </FormControl>
+    );
+  };
+
   // 컬럼 정의
   const columns = [
-    // { field: "id", headerName: "ID", width: 100 },
-    { field: "loginId", headerName: "로그인 아이디", width: 150 },
-    // { field: "password", headerName: "패스워드", width: 150 },
-    { field: "agencyName", headerName: "대행사명", width: 150 },
-    { field: "name", headerName: "담당자", width: 150 },
-    { field: "phoneNum", headerName: "연락처", width: 150 },
-    { field: "placeTraffic", headerName: "플레이스 트래픽", width: 150 },
-    { field: "placeSave", headerName: "플레이스 저장", width: 150 },
+    { field: "loginId", headerName: "로그인 아이디", width: 170 },
+    { field: "agencyName", headerName: "대행사명", width: 170, editable: true },
+    { field: "name", headerName: "담당자", width: 150, editable: true },
+    { field: "phoneNum", headerName: "연락처", width: 150, editable: true },
+    {
+      field: "placeTraffic",
+      headerName: "플레이스 트래픽",
+      width: 130,
+      editable: true,
+    },
+    {
+      field: "placeSave",
+      headerName: "플레이스 저장",
+      width: 130,
+      editable: true,
+    },
     {
       field: "placeSavePremium",
       headerName: "플레이스 저장 프리미엄",
-      width: 200,
+      width: 130,
+      editable: true,
     },
-    { field: "resaleYn", headerName: "재판매 여부", width: 150 },
-    // { field: "userType", headerName: "사용자 여부", width: 150 },
-    // { field: "useYn", headerName: "사용 여부", width: 150 },
-    // { field: "registerDateTime", headerName: "등록일자", width: 150 },
+    {
+      field: "resaleYn",
+      headerName: "재판매 여부",
+      width: 100,
+      editable: true,
+      renderEditCell: renderDropdownEditCell, // 드롭다운 렌더링 함수 지정
+    },
+    {
+      field: "useYn",
+      headerName: "사용 여부",
+      width: 100,
+      editable: true,
+      renderEditCell: renderDropdownEditCell, // 드롭다운 렌더링 함수 지정
+    },
   ];
+
+  const getRowClassName = (params) => {
+    return updatedRows[params.id] ? "updated-row" : "";
+  };
 
   return (
     <div className="mainContainerDiv" style={{ height: 600, width: "100%" }}>
       <div className="missionManageDiv">
         <h2 className="menuTitle">대행사 관리</h2>
 
-        <div>
+        <div className="actionBtns">
           <button type="button" onClick={addAgency}>
-            대행사 추가
+            New
           </button>
+          <button type="button" onClick={agencySave}>
+            Save
+          </button>
+          <button type="button">Excel</button>
         </div>
-
-        {status === "loading" && <p>Loading...</p>}
-        {status === "failed" && <p>Error: {error}</p>}
-
         <DataGrid
           rows={rows}
           columns={columns}
-          getRowId={(row) => row.id}
+          processRowUpdate={processRowUpdate}
+          getRowClassName={getRowClassName} // 행 클래스 이름 설정
           initialState={{
             pagination: {
               paginationModel: { page: 0, pageSize: 5 },
--- src/component/mng/NovaMissionManage.jsx
+++ src/component/mng/NovaMissionManage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { useNavigate } from "react-router-dom";
 import "../../css/Common.css";
@@ -8,6 +8,16 @@ import { api } from "../../api/api.js";
 const NovaMissionManage = () => {
   const navigate = useNavigate();
   const [activeType, setActiveType] = useState("전체");
+  const [rows, setRows] = useState([]);
+  const [pointsData, setPointsData] = useState({
+    currentPoints: 0,
+    placeTraffic: 0,
+    placeSave: 0,
+    placeSavePremium: 0,
+    placeTrafficPremium: 0,
+  });
+
+  const [constName, setConstName] = useState("");
 
   const handleTypeClick = (type) => {
     setActiveType(type);
@@ -17,75 +27,71 @@ const NovaMissionManage = () => {
     navigate("/main/addMission");
   };
 
-  const onSubmit = async () => {
+  const getAgencyMissionListByAgencyName = async () => {
     try {
-      const response = await api.get("/mission/test"); // GET 요청
-      console.log(response.data); // 응답 데이터를 출력
+      const response = await api.get(
+        "/mission/getAgencyMissionListByAgencyName",
+        {
+          params: { agencyName: constName },
+        }
+      );
+      const data = response.data;
+      const formattedData = data.map((el, index) => ({
+        id: index + 1,
+        agencyName: el.agencyName || "N/A",
+        kind: el.missionType || "N/A",
+        type: el.missionCategory || "N/A",
+        mid: el.placeMid || "N/A",
+        adStartDate: el.adStartDate || "N/A",
+        dailyWorkload: el.dailyWorkload || "N/A",
+        totalWorkdays: el.totalWorkdays || "N/A",
+        rankKeyword: el.rankKeyword || "N/A",
+        searchKeyword: el.searchKeyword || "N/A",
+        adEndDate: el.adEndDate || "N/A",
+        placeAddress: el.placeAddress || "N/A",
+        totalRequests: el.totalRequest || "N/A",
+        missionStatus: el.missionStatus || "N/A",
+      }));
+      setRows(formattedData);
+      // 대행사 포인트 조회
+      getAgencyPointByAgencyName();
     } catch (error) {
       console.error("API 호출 중 오류가 발생했습니다:", error);
     }
   };
 
-  const rows = [
-    {
-      id: 1,
-      kind: "종류1",
-      type: "유형1",
-      mid: "MID1",
-      adStartDate: "2024-07-31",
-      dailyWorkload: "100P",
-      workDate: "2024-08-01",
-      rankKeyword: "키워드1",
-      searchKeyword: "검색어1",
-      adEndDate: "2024-08-31",
-      placeAddress: "주소1",
-      totalRequests: "200P",
-      missionStatus: "완료",
-      manage: "관리1",
-    },
-    {
-      id: 2,
-      kind: "종류2",
-      type: "유형2",
-      mid: "MID2",
-      adStartDate: "2024-07-31",
-      dailyWorkload: "200P",
-      workDate: "2024-08-01",
-      rankKeyword: "키워드2",
-      searchKeyword: "검색어2",
-      adEndDate: "2024-08-31",
-      placeAddress: "주소2",
-      totalRequests: "300P",
-      missionStatus: "진행중",
-      manage: "관리2",
-    },
-    {
-      id: 3,
-      kind: "종류3",
-      type: "유형3",
-      mid: "MID3",
-      adStartDate: "2024-07-31",
-      dailyWorkload: "150P",
-      workDate: "2024-08-01",
-      rankKeyword: "키워드3",
-      searchKeyword: "검색어3",
-      adEndDate: "2024-08-31",
-      placeAddress: "주소3",
-      totalRequests: "250P",
-      missionStatus: "대기중",
-      manage: "관리3",
-    },
-  ];
+  const getAgencyPointByAgencyName = async () => {
+    try {
+      const response = await api.get("/point/getAgencyPointByAgencyName", {
+        params: { agencyName: constName },
+      });
 
-  // 컬럼 정의
+      const data = response.data[0];
+
+      setPointsData({
+        currentPoints: response.data.length > 1 ? 0 : data.availablePoints,
+        placeTraffic: response.data.length > 1 ? 0 : data.placeTraffic,
+        placeSave: response.data.length > 1 ? 0 : data.placeSave,
+        placeSavePremium: response.data.length > 1 ? 0 : data.placeSavePremium,
+      });
+    } catch (error) {
+      console.error("error:", error);
+    }
+  };
+
+  useEffect(() => {
+    getAgencyMissionListByAgencyName();
+  }, []);
+
   const columns = [
     { field: "id", headerName: "No", width: 30 },
+    { field: "agencyName", headerName: "대행사", width: 50 },
     { field: "kind", headerName: "종류", width: 100 },
     { field: "type", headerName: "유형", width: 100 },
     { field: "mid", headerName: "mid", width: 100 },
     { field: "adStartDate", headerName: "광고시작일", width: 100 },
     { field: "dailyWorkload", headerName: "1일작업량", width: 100 },
-    { field: "workDate", headerName: "작업일", width: 100 },
+    { field: "totalWorkdays", headerName: "총작업일수", width: 100 },
     { field: "rankKeyword", headerName: "순위키워드", width: 100 },
     { field: "searchKeyword", headerName: "검색키워드", width: 100 },
     { field: "adEndDate", headerName: "광고종료일", width: 100 },
@@ -102,12 +108,16 @@ const NovaMissionManage = () => {
         <div className="searchDiv">
           <div>
             <h3>검색어</h3>
-            <select>
-              <option>상품명</option>
-              <option>대행사명</option>
-            </select>
-            <input type="text" />
-            <button onClick={onSubmit}>검색</button>
+            <input
+              type="text"
+              id="constName"
+              placeholder="대행사명"
+              onChange={(e) => setConstName(e.target.value)}
+              onKeyDown={(e) =>
+                e.key === "Enter" && getAgencyMissionListByAgencyName()
+              }
+            />
+            <button onClick={getAgencyMissionListByAgencyName}>검색</button>
           </div>
           <div>
             <b>타입</b>
@@ -156,21 +166,21 @@ const NovaMissionManage = () => {
             </thead>
             <tbody>
               <tr>
-                <td>867,250P</td>
-                <td>35P</td>
-                <td>25P</td>
-                <td>35P</td>
-                <td>45P</td>
+                <td>{pointsData.currentPoints}P</td>
+                <td>{pointsData.placeTraffic}P</td>
+                <td>{pointsData.placeSave}P</td>
+                <td>{pointsData.placeSavePremium}P</td>
+                <td>{pointsData.placeTrafficPremium}P</td>
               </tr>
             </tbody>
           </table>
         </div>
 
-        <div styleName="addMissionBtnDiv">
-          <button type="button">엑셀 다운로드</button>
+        <div className="actionBtns">
           <button type="button" onClick={addMission}>
-            추가
+            미션 추가
           </button>
+          <button type="button">엑셀 다운로드</button>
         </div>
 
         <DataGrid
