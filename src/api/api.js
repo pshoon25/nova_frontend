6817797a8b85ca6b16620546cd5d39152166e395
pshoon25 <pshoon25@naver.com>
2024-09-20 22:24:35 +0900

    추석 공지 삭제

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const url = "http://localhost:8080";
-const url = "/api";
+const url = "http://localhost:8080";
+// const url = "/api";
 
 const loginInfo = JSON.parse(localStorage.getItem("loginInfo"));
 const accessToken = loginInfo ? loginInfo.accessToken : null;
